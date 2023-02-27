05bf7653a9eb94eee74ea13f0388da855dfe398b
prince4mayor <princemayor32@gmail.com>
2023-02-27 14:51:03 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* main - print _putchar
-* is written by mbah
-* Return: 0
-*/
+ * main - Entry point
+ * Description: prints "_putchar"
+ * Return: 0
+ */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
